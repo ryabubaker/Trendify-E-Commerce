8978969a26f1ab95c048b1e297262d82bd97f52b
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-08-26 21:32:57 +0300

    design product card

---
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../core/models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+  calculateDiscount(originalPrice: number, discountedPrice: number): number {
+    return Math.round(
+      ((originalPrice - discountedPrice) / originalPrice) * 100
+    );
+  }
+}
