036273b157addd644848d489f492f608c3cc2e8c
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-08-27 15:22:13 +0300

    add environment

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
   private readonly http = inject(HttpClient);
 
   getAllProducts(): Observable<any> {
-    return this.http.get('https://ecommerce.routemisr.com/api/v1/products');
+    return this.http.get(environment.baseUrl + 'products');
   }
 }
