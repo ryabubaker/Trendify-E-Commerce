e773d0afb6587afc876a15531c18123059e79a7b
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-08-27 23:00:30 +0300

    fixed responsiveness of footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FlowbiteService } from '../../../core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  private readonly flowbiteService = inject(FlowbiteService);
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
