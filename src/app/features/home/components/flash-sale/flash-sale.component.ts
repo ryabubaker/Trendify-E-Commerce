28e48d2a35bdc0947ed482c75240d6eb530d8e8c
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-09-11 21:00:53 +0300

    add flash sale section with functionality

---
+++ src/app/features/home/components/flash-sale/flash-sale.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Product } from '../../../../core/models/product.model';
+import { ProductsService } from '../../../../core/services/products/products.service';
+import { ProductCardComponent } from '../../../../shared/components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-flash-sale',
+  imports: [ProductCardComponent],
+  templateUrl: './flash-sale.component.html',
+  styleUrl: './flash-sale.component.css',
+})
+export class FlashSaleComponent implements OnInit {
+  private readonly productsService = inject(ProductsService);
+  productList: Product[] = [];
+
+  ngOnInit(): void {
+    this.getAllProductsData();
+  }
+
+  getAllProductsData(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: (response) => {
+        this.productList = response.data;
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      },
+    });
+  }
+
+  get discountedProducts() {
+    return this.productList
+      .filter((p) => (p.priceAfterDiscount ?? 0) > 0)
+      .slice(0, 4);
+  }
+}
