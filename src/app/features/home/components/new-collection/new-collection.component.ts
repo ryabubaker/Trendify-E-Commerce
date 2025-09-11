3dd951d399752e67aa80ce60ded7097aaa8fe892
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-09-11 21:02:04 +0300

    added new collection section

---
+++ src/app/features/home/components/new-collection/new-collection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-collection',
+  imports: [],
+  templateUrl: './new-collection.component.html',
+  styleUrl: './new-collection.component.css'
+})
+export class NewCollectionComponent {
+
+}
