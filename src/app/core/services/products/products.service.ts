ee308b223fff7cc4b6668f5b552746b9678296ca
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2025-09-11 20:58:33 +0300

    edited product card and added getProducts function that can take params

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
@@ -11,5 +11,17 @@ export class ProductsService {
 
   getAllProducts(): Observable<any> {
     return this.http.get(environment.baseUrl + 'products');
+  }
+
+  getProducts(params?: Record<string, any>): Observable<any> {
+    let httpParams = new HttpParams();
+    
+    if (params) {
+      Object.keys(params).forEach(key => {
+        httpParams = httpParams.set(key, params[key]);
+      });
+    }
+
+    return this.http.get<any>(environment.baseUrl + 'products', { params: httpParams });
   }
 }
